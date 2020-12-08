251e2799e0ed45651b1c08cb2ccc2353c9dd453c
Horak2084 <horak@live.at>
2020-12-08 13:07:50 +0000

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"fordpass adapter settings": {
+		"en": "Adapter settings for fordpass",
+		"de": "Adaptereinstellungen für fordpass",
+		"ru": "Настройки адаптера для fordpass",
+		"pt": "Configurações do adaptador para fordpass",
+		"nl": "Adapterinstellingen voor fordpass",
+		"fr": "Paramètres d'adaptateur pour fordpass",
+		"it": "Impostazioni dell'adattatore per fordpass",
+		"es": "Ajustes del adaptador para fordpass",
+		"pl": "Ustawienia adaptera dla fordpass",
+		"zh-cn": "fordpass的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "option2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
